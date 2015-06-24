e8270ab4e4ca63937491c2823b043a6c8863d848
cliffchew84 <cliffchew84@gmail.com>
2015-06-25 00:33:58 +0800

    Edited the 2 tor scripts

--- tor_id_single_use.py
+++ tor_id_single_use.py
@@ -7,7 +7,7 @@ socket.socket = socks.socksocket
 
 # BeautifulSoup magic --> Copied from my previous work
 user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'User-Agent': user_agent}
+heads = {'User-Agent': user_agent}
 
 req = urllib2.Request("http://whatismyipaddress.com/", headers=heads)
 url_f = urllib2.urlopen(req)
@@ -15,6 +15,3 @@ soup = BeautifulSoup(url_f)
 
 for i in soup.find_all("a",style="font-weight:bold;color:#007cc3;font-size:26px;text-decoration: none;"):
     print i.text
-
-# For some reason, I am unable to use the ip address below...
-# print(urllib2.urlopen("http://www.ifconfig.me/ip").read())
