083f2884d344f447d476e0494da8c0bb153fa47d
cliffchew84 <cliffchew84@gmail.com>
2015-06-24 23:52:29 +0800

    Added 2 tor scripts

---
+++ tor_id_refresh.py
@@ -0,0 +1,38 @@
+import socks, socket, urllib2, requests
+from bs4 import BeautifulSoup
+from stem import Signal
+from stem.control import Controller
+
+with Controller.from_port(port = 9151) as controller:
+    controller.authenticate()
+    controller.signal(Signal.NEWNYM)
+
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
+    socket.socket = socks.socksocket
+
+    user_agent = 'Mozilla/5.0'
+    heads = {'User-Agent': user_agent}
+
+    req = urllib2.Request("http://whatismyipaddress.com/", headers=heads)
+    url_f = urllib2.urlopen(req)
+    soup = BeautifulSoup(url_f)
+
+    for i in soup.find_all("a",style="font-weight:bold;color:#007cc3;font-size:26px;text-decoration: none;"):
+        print i.text
+
+    controller.signal(Signal.HUP) # Resetting IP address through Tor
+
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
+    socket.socket = socks.socksocket
+
+    # Using BeautifulSoup again
+    user_agent = 'Mozilla/5.0'
+    heads = {'User-Agent': user_agent}
+
+    req = urllib2.Request("http://whatismyipaddress.com/", headers=heads)
+    url_f = urllib2.urlopen(req)
+    soup = BeautifulSoup(url_f)
+
+    for i in soup.find_all("a",style="font-weight:bold;color:#007cc3;font-size:26px;text-decoration: none;"):
+        print i.text
+
---
+++ tor_id_single_use.py
@@ -0,0 +1,20 @@
+''' Python Tor '''
+import socks, socket, urllib2, requests
+
+# Tor magic --> Remember to open Tor Browser
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
+socket.socket = socks.socksocket
+
+# BeautifulSoup magic --> Copied from my previous work
+user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent': user_agent}
+
+req = urllib2.Request("http://whatismyipaddress.com/", headers=heads)
+url_f = urllib2.urlopen(req)
+soup = BeautifulSoup(url_f)
+
+for i in soup.find_all("a",style="font-weight:bold;color:#007cc3;font-size:26px;text-decoration: none;"):
+    print i.text
+
+# For some reason, I am unable to use the ip address below...
+# print(urllib2.urlopen("http://www.ifconfig.me/ip").read())
